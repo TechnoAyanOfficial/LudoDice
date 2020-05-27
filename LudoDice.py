e77a4631edb6090d9c53a4787e060d8bde4c0295
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-05-27 10:55:00 +0530

    Update LudoDice.py

--- LudoDice.py
+++ LudoDice.py
@@ -1,29 +1,30 @@
 """
-This is a echo bot.
-It echoes any incoming text messages.
+TinyurlBoT
 """
 
 import logging
-
+import pyshorteners
 from aiogram import Bot, Dispatcher, executor, types
 
-import random 
 
-API_TOKEN = '1269853254:AAH9C3JGFYZouqPaIfAnOefW6lq4857Ddnw'
+API_TOKEN = '1099402493:AAGVZgwWlgUOX763ALt2qfIIwxmtmq7gGDc'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
-# Initialize bot and dispatcher
+# Initialize bot 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+s = pyshorteners.Shortener()
+welcome_msg = "👋 Hi there i'm Tinyurl Bot !!\n🤖 This bot can short any link\n🔥 Fastest url shortener Bot\n🔗 Just send me your link"
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.reply(random.choice([1, 2, 3, 4, 5, 6]))
+	await message.answer(welcome_msg)
+
+@dp.message_handler()
+async def echo(message: types.Message):
+	await message.answer('Here Is Your Shortened Url👇\n\n' + (s.tinyurl.short(message.text)))
 
 
 if __name__ == '__main__':
